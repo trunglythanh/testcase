19d0c2f8c29045e0229e4cd69d292ce69228fb9a
trung_ly <trung.ly@axonactive.com>
2021-06-01 11:53:48 +0700

    sprint 2106A

---
+++ Sprint2106A.mm
@@ -0,0 +1,51 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1622520265138" ID="ID_311051902" MODIFIED="1622520287543" TEXT="Sprint 21.06.A">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1622522301940" ID="ID_777361456" MODIFIED="1622522307288" POSITION="right" TEXT="AF-63762">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1622522323014" ID="ID_725801295" MODIFIED="1622522338302" TEXT="CR | Automated Rating">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1622522343481" ID="ID_197990432" MODIFIED="1622522347366" TEXT="Case1">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1622522350469" ID="ID_768838328" MODIFIED="1622522377194" TEXT="Choose Living, Holiday and Aluxury">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1622522377536" ID="ID_1933421175" MODIFIED="1622522391554" TEXT="After short analysis">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1622522400139" ID="ID_80075619" MODIFIED="1622522678805" TEXT="Auto rating"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1622522690211" ID="ID_1297154800" MODIFIED="1622522694375" TEXT="Case 1.1">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1622522699056" ID="ID_169098196" MODIFIED="1622522725365" TEXT="Request for approval">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1622522707574" ID="ID_1730765738" MODIFIED="1622522711490" TEXT="Check out">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1622522728602" ID="ID_270729755" MODIFIED="1622522730737" TEXT="MUST">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1622522734868" ID="ID_1660976076" MODIFIED="1622522747288" TEXT="Request Crif and ZEK"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1622522628540" ID="ID_486232587" MODIFIED="1622522631305" TEXT="Case 2">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1622522641110" ID="ID_1596962209" MODIFIED="1622522648566" TEXT="Choose other ">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1622522648876" ID="ID_218871209" MODIFIED="1622522658241" TEXT="After short analysis">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1622522660798" ID="ID_976158761" MODIFIED="1622522672349" TEXT="No auto rating"/>
+</node>
+</node>
+</node>
+</node>
+</map>
